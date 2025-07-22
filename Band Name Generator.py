a171776ae2ff57bb662b4e64aee20a704a60619a
rmadhulatha <madhulatha1690@gmail.com>
2025-07-22 22:21:01 +0530

    new committed

---
+++ Band Name Generator.py
@@ -0,0 +1,8 @@
+print("Welcome to the Band Name Generator")
+city = input("which city did you grew up in? :\n")
+print(city)
+pet = input("what is the name of pet? :\n")
+print(pet)
+print("Your band name could be:" + city + " " + pet )
+
+
--- python-string.py
+++
@@ -1,7 +0,0 @@
-print("hellow welcome to my git repo")
-
-print("this is remote repo")
-
-print("hwllo this is from remote repo")
-
-
